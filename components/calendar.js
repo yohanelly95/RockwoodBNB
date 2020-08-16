82e55e71afce95141afe41ed9fd0e050833add58
yohanelly95 <yohan.nelson@signeasy.com>
2020-08-16 15:42:06 +0530

    general fixes || blocker found

--- components/calendar.js
+++ components/calendar.js
@@ -1,30 +1,33 @@
-import { useState, useEffect, useRef } from "react";
-import Calendar from "react-calendar";
-import moment from "moment";
+import { useState, useEffect, useRef } from 'react';
+import Calendar from 'react-calendar';
+import moment from 'moment';
 
 const CalendarComponent = (props) => {
-  const [fromDate, setFromDate] = useState("");
 
-  useEffect(() => {
-    const { getCalendarDate } = props;
-    const showSelectedFromDate =
-      fromDate === "" ? "" : moment(fromDate).format("DD/MM/YYYY");
-    getCalendarDate(showSelectedFromDate);
-  }, [fromDate]);
+    const [fromDate, setFromDate] = useState('');
 
-  return (
-    <React.Fragment>
-      <Calendar
-        onChange={setFromDate}
-        value={fromDate}
-        minDate={new Date()}
-        showNeighboringMonth={false}
-        maxDetail="month"
-        next2Label={null}
-        prev2Label={null}
-      />
-    </React.Fragment>
-  );
-};
+
+    useEffect(() => {
+        const { getCalendarDate } = props;
+        const showSelectedFromDate = fromDate === '' ? "" : moment(fromDate).format('DD/MM/YYYY');
+        getCalendarDate(showSelectedFromDate);
+    }, [fromDate]);
+
+
+
+    return(
+        <React.Fragment>
+            <Calendar
+                onChange={setFromDate}
+                value={fromDate}
+                minDate={new Date()}
+                showNeighboringMonth={true}
+                maxDetail="month"
+                next2Label={null}
+                prev2Label={null}
+            />
+        </React.Fragment>
+    )
+}
 
 export default CalendarComponent;
