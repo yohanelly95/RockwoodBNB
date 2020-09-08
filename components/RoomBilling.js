0d0e95f51c092a08312b4a153a1a6c0236fa31c9
yohanelly95 <yohan.nelson@signeasy.com>
2020-09-08 22:47:41 +0530

    express server integration

--- components/RoomBilling.js
+++ components/RoomBilling.js
@@ -6,7 +6,7 @@ const RoomBilling = (props) => {
     const { fromDate, toDate, numberOfGuest, numberOfRooms } = props;
 
     //Total Calc can be done here
-
+    
     return(
         <>
         <h2 className="mt-4">Room 3</h2>
@@ -14,7 +14,7 @@ const RoomBilling = (props) => {
                 <div className="">
                     <p className="text-2xl">₹<span>1999</span><span className="text-base"> /night</span></p>
                     <p className="mt-2"><span>{fromDate}</span> to <span>{toDate}</span></p>
-                    <p className="mt-6">₹<span>1999</span> x <span>2</span><span className="float-right">₹<span>3998</span></span></p>
+                    <p className="mt-6">₹<span>1999</span> x <span>{numberOfRooms}</span><span className="float-right">₹<span>3998</span></span></p>
                     <p className="mt-2">Extra Bedding<span className="float-right">₹<span>750</span></span></p>
                     <p className="mt-4 font-bold">Total<span className="float-right">₹<span>4748</span></span></p>
 
